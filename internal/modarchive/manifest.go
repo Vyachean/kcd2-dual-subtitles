67fd1b005b7297d7ca0ef48206476124c32e0c5d
Vyachean <66068072+Vyachean@users.noreply.github.com>
2026-08-23 19:00:51 +0400

    feat: version generated mod manifest

--- internal/modarchive/manifest.go
+++ internal/modarchive/manifest.go
@@ -1,17 +1,38 @@
 package modarchive
 
+import "strings"
+
 const ModID = "kcd_dual_subtitles"
 
-const manifest = `<?xml version="1.0" encoding="utf-8"?>
+func manifestForVersion(version string) []byte {
+	version = strings.TrimSpace(version)
+	if version == "" {
+		version = "dev"
+	}
+	version = escapeXMLText(version)
+
+	return []byte(`<?xml version="1.0" encoding="utf-8"?>
 <kcd_mod>
     <info>
         <name>KCD2 Dual Subtitles</name>
         <modid>` + ModID + `</modid>
         <description>Generated bilingual subtitle localization.</description>
         <author>Vyachean</author>
-        <version>0.1.0</version>
+        <version>` + version + `</version>
         <created_on>2026-08-23</created_on>
         <modifies_level>false</modifies_level>
     </info>
 </kcd_mod>
-`
+`)
+}
+
+func escapeXMLText(value string) string {
+	replacer := strings.NewReplacer(
+		"&", "&amp;",
+		"<", "&lt;",
+		">", "&gt;",
+		`"`, "&quot;",
+		"'", "&apos;",
+	)
+	return replacer.Replace(value)
+}
