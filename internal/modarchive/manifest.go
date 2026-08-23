dea52551ea6bac0d58a2b1d716c204ea816ea53b
Vyachean <66068072+Vyachean@users.noreply.github.com>
2026-08-23 15:23:10 +0400

    feat: add deterministic KCD2 mod archive builder (#13)
    * feat: add deterministic mod manifest
    
    * feat: add deterministic mod archive builder
    
    * test: cover nested mod archive generation

---
+++ internal/modarchive/manifest.go
@@ -0,0 +1,15 @@
+package modarchive
+
+const manifest = `<?xml version="1.0" encoding="utf-8"?>
+<kcd_mod>
+    <info>
+        <name>KCD2 Dual Subtitles</name>
+        <modid>kcd2dualsubtitles</modid>
+        <description>Generated bilingual subtitle localization.</description>
+        <author>Vyachean</author>
+        <version>0.1.0</version>
+        <created_on>23 Aug 2026</created_on>
+        <modifies_level>false</modifies_level>
+    </info>
+</kcd_mod>
+`
