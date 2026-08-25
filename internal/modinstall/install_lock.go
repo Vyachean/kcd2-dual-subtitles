b41f1c3c472180c9b28e26b4557650bc1114e0b8
Vyachean <66068072+Vyachean@users.noreply.github.com>
2026-08-25 11:43:06 +0400

    refactor: serialize all mod mutations

--- internal/modinstall/install_lock.go
+++ internal/modinstall/install_lock.go
@@ -2,4 +2,4 @@ package modinstall
 
 import "errors"
 
-var ErrInstallInProgress = errors.New("another KCD2 Dual Subtitles installation is already in progress")
+var ErrInstallInProgress = errors.New("another KCD2 Dual Subtitles mod operation is already in progress")
