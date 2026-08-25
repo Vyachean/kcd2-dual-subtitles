1ccb0e94e8e661940a3c8a556dfe09e8e2369a34
Vyachean <66068072+Vyachean@users.noreply.github.com>
2026-08-25 11:45:20 +0400

    refactor: name shared mod-operation lock

--- internal/modinstall/install_lock.go
+++ internal/modinstall/install_lock.go
@@ -2,4 +2,4 @@ package modinstall
 
 import "errors"
 
-var ErrInstallInProgress = errors.New("another KCD2 Dual Subtitles mod operation is already in progress")
+var ErrModOperationInProgress = errors.New("another KCD2 Dual Subtitles mod operation is already in progress")
