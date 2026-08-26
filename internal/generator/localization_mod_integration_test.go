b23c92e44806263f942bcffac1a828ffbeea6ebe
Vyachean <66068072+Vyachean@users.noreply.github.com>
2026-08-26 21:21:27 +0400

    test: use valid KCD mod ID in generator integration

--- internal/generator/localization_mod_integration_test.go
+++ internal/generator/localization_mod_integration_test.go
@@ -19,7 +19,7 @@ func TestGenerateUsesActiveLocalizationModAsMainSource(t *testing.T) {
 	if err := os.MkdirAll(filepath.Join(modDir, "Localization"), 0o755); err != nil {
 		t.Fatal(err)
 	}
-	if err := os.WriteFile(filepath.Join(modDir, "mod.manifest"), []byte(`<?xml version="1.0"?><kcd_mod><info><name>Russian Fix</name><modid>russian-fix</modid></info></kcd_mod>`), 0o600); err != nil {
+	if err := os.WriteFile(filepath.Join(modDir, "mod.manifest"), []byte(`<?xml version="1.0"?><kcd_mod><info><name>Russian Fix</name><modid>russian_fix</modid></info></kcd_mod>`), 0o600); err != nil {
 		t.Fatal(err)
 	}
 	writeLanguagePAK(t, filepath.Join(modDir, "Localization", "Russian_xml.pak"), `<?xml version="1.0" encoding="utf-8"?>
